896ae1c60320c59a1f5fc1d2ffd18bd808640d32
Aaron Chapin <achapin@goodgamestudios.com>
2021-05-26 00:12:56 +0200

    Clamp max dimensions so panel contents don't cause the panels to change size

--- tuckbox.js
+++ tuckbox.js
@@ -58,7 +58,7 @@ function addShip(){
 
 function getShipTable(shipToParse, faction, id){
 	var ship = JSON.parse(shipToParse);
-	var display = `<div class="icon">${faction}</div>
+	var display = `<div class="icon shipFaction">${faction}</div>
                 <div class="fancy">${ship.name}</div>
                 <div class="ship">${ship.icon}</div>`
 
@@ -66,48 +66,48 @@ function getShipTable(shipToParse, faction, id){
 	<span class="removeButton no-print" onclick="remove(${id})">Remove</span>
 	<table>
     <tr class="top-flap">
-        <td class="model-height" style="width:${ship.height}mm;"></td>
-        <td class="model-width render-flap cut-top cut-left cut-right" style="width:${ship.width}mm;"></td>
+        <td class="model-height" style="width:${ship.height}mm; max-width:${ship.height}mm;"></td>
+        <td class="model-width render-flap cut-top cut-left cut-right" style="width:${ship.width}mm; max-width:${ship.width}mm;"></td>
         <td colspan="3"></td>
     </tr>
-    <tr class="top model-height-height" style="height:${ship.height}mm;">
-        <td class="model-height flap-left cut-left cut-right cut-top" style="width:${ship.height}mm; border-top-left-radius:${ship.height}mm;"></td>
-        <td class="model-width render cut-left cut-right" style="width:${ship.width}mm;">
-            <div class="upsideDown">
+    <tr class="top model-height-height" style="height:${ship.height}mm; max-height:${ship.height}mm;">
+        <td class="model-height flap-left cut-left cut-right cut-top" style="width:${ship.height}mm; border-top-left-radius:${ship.height}mm; max-width:${ship.height}mm;"></td>
+        <td class="model-width render cut-left cut-right" style="width:${ship.width}mm; max-width:${ship.width}mm;">
+            <div class="upsideDown content">
                 ${display}
             </div>
         </td>
-        <td class="model-height flap-right cut-left cut-right cut-top" style="width:${ship.height}mm; border-top-right-radius:${ship.height}mm;"></td>
+        <td class="model-height flap-right cut-left cut-right cut-top" style="width:${ship.height}mm; border-top-right-radius:${ship.height}mm; max-width:${ship.height}mm;"></td>
         <td  colspan="2"></td>
     </tr>
-    <tr class="main model-length"  style="height:${ship.length}mm;">
-        <td class="model-height render cut-left style="width:${ship.height}mm;"">
+    <tr class="main model-length"  style="height:${ship.length}mm; max-height:${ship.length}mm;">
+        <td class="model-height render cut-left" style="width:${ship.height}mm; max-width:${ship.height}mm;">
             <div class="leftSide">
-                ${display}
+            	${display}
             </div>
         </td>
-        <td class="model-width render" style="width:${ship.width}mm;">
+        <td class="model-width render" style="width:${ship.width}mm; max-width:${ship.width}mm;">
             ${display}
         </td>
-        <td class="model-height render style="width:${ship.height}mm;"">
+        <td class="model-height render" style="width:${ship.height}mm; max-width:${ship.height}mm;">
             <div class="rightSide">
-                ${display}
+            ${display}
             </div>
         </td>
-        <td class="model-width render cut-top" style="width:${ship.width}mm;">
+        <td class="model-width render cut-top" style="width:${ship.width}mm; max-width:${ship.width}mm;">
             ${display}
         </td>
         <td class="side-flap render cut-top cut-bottom cut-right"></td>
         <td></td>
     </tr>
     <tr class="model-height-height" style="height:${ship.height}mm;">
-        <td class="model-height render cut-left cut-bottom cut-right" style="width:${ship.height}mm;">
+        <td class="model-height render cut-left cut-bottom cut-right" style="width:${ship.height}mm; max-width:${ship.height}mm;">
         </td>
-        <td class="model-width render cut-left cut-bottom cut-right" style="width:${ship.width}mm;">
+        <td class="model-width render cut-left cut-bottom cut-right" style="width:${ship.width}mm; max-width:${ship.width}mm;">
         </td>
-        <td class="model-height render cut-left cut-bottom cut-right" style="width:${ship.height}mm;">
+        <td class="model-height render cut-left cut-bottom cut-right" style="width:${ship.height}mm; max-width:${ship.height}mm;">
         </td>
-        <td class="model-width render cut-left cut-bottom cut-right" style="width:${ship.width}mm;">
+        <td class="model-width render cut-left cut-bottom cut-right" style="width:${ship.width}mm; max-width:${ship.width}mm;">
         </td>
         <td></td>
     </tr>
