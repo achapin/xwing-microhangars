da01c68cc18f61d5e418dd44ed0acef8d4e28e94
Aaron Chapin <achapin@goodgamestudios.com>
2021-09-08 21:47:32 +0200

    Layout for large ships

--- tuckbox.js
+++ tuckbox.js
@@ -152,6 +152,49 @@ function getShipTable(shipToParse, faction, id){
      	sideRightOffset = ship.sideRightOffset;
     }
 
+    if(ship.size == "large")
+    {
+        return `<div id="ship-${id}" class="box">
+        <span class="removeButton no-print" onclick="remove(${id})">Remove</span> <span class="no-print">${warning}</span>
+        <table>
+            <tr class="large-left" style="height:${ship.height}mm; max-height:${ship.height}mm;">
+                <td class="left-bottom-flap glue cut-left cut-top cut-right" style="width:${ship.height}mm; max-width:${ship.height}mm;"> </td>
+                <td class="left-side render cut-top" style="width:${ship.length}mm; max-width:${ship.length}mm;">LEFT</td>
+                <td class="left-top-flap render cut-top cut-right" style="width:${ship.height}mm; max-width:${ship.height}mm;"></td>
+                <!--Spacer here?-->
+            </tr>
+            <tr class="large-center" style="height:${ship.width}mm; max-height:${ship.width}mm;">
+                <td class="bottom render cut-left cut-top cut-bottom" style="width:${ship.height}mm; max-width:${ship.height}mm;">BOTTOM</td>
+                <td class="front render" style="width:${ship.length}mm; max-width:${ship.length}mm;">FRONT</td>
+                <td class="top render cut-top cut-bottom" style="width:${ship.height}mm; max-width:${ship.height}mm;">TOP</td>
+                <td class="top-flap render cut-top cut-right cut-bottom side-flap"></td>
+            </tr>
+            <tr class="large-right" style="height:${ship.height}mm; max-height:${ship.height}mm;">
+                <td class="right-bottom-flap glue cut-top cut-left cut-bottom" style="width:${ship.height}mm; max-width:${ship.height}mm;"></td>
+                <td class="right-side render cut-bottom" style="width:${ship.length}mm; max-width:${ship.length}mm;">RIGHT</td>
+                <td class="right-top-flap render cut-bottom cut-top cut-right" style="width:${ship.height}mm; max-width:${ship.height}mm;"></td>
+                <!--Spacer here?-->
+            </tr>
+        </table>
+        <table>
+            <tr class="large-left" style="height:${ship.height}mm; max-height:${ship.height}mm;">
+                <td class="bottom-spacer" style="width:${ship.height}mm; max-width:${ship.height}mm;"></td>
+                <td class="left-side-flap glue cut-top cut-right cut-bottom cut-left"></td>
+                <!--Spacer here?-->
+            </tr>
+            <tr class="large-center" style="height:${ship.width}mm; max-height:${ship.width}mm;">
+                <td class="bottom-flap glue cut-left cut-top cut-bottom" style="width:${ship.height}mm; max-width:${ship.height}mm;"></td>
+                <td class="back render cut-top cut-right cut-bottom" style="width:${ship.length}mm; max-width:${ship.length}mm;">BACK</td>
+            </tr>
+            <tr class="large-left" style="height:${ship.height}mm; max-height:${ship.height}mm;">
+                <td class="bottom-spacer" style="width:${ship.height}mm; max-width:${ship.height}mm;"></td>
+                <td class="right-side-flap glue cut-left cut-right cut-bottom"></td>
+                <!--Spacer here?-->
+            </tr>
+        </table>
+        </div>`;
+    }
+
 	return `<div id="ship-${id}" class="box">
 	<span class="removeButton no-print" onclick="remove(${id})">Remove</span> <span class="no-print">${warning}</span>
 	<table>
