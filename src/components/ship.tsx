6171423cb7c462a75c7abe60eccbfe77a63ca73e
Phillip Manwaring <phillip.manwaring@gmail.com>
2021-09-15 23:28:12 -0400

    initial react setup

--- src/components/ship.tsx
+++ src/components/ship.tsx
@@ -7,6 +7,9 @@ export interface ShipInterface {
   width: number;
   length: number;
   sideLeftOffset: number;
+  sideRightOffset: number;
+  displaySide: boolean;
+  displayTop: boolean;
   displayFactionSize: number;
   displayIconSize: number;
   displaySideFactionSize: number;
@@ -16,7 +19,13 @@ export interface ShipInterface {
   factions: string[];
 }
 
-export const Ship = ({ ship }: { ship: ShipInterface }) => {
+export interface FactionInterface {
+  name: string;
+  icon: string;
+  color: string;
+}
+
+export const Ship = ({ ship, faction, useFactionColor }: { ship: ShipInterface, faction: FactionInterface, useFactionColor: boolean }) => {
 
   const shipHeight = `${ship.height}mm`;
   const shipHeightPlus = `${ship.height + 2}mm`
@@ -24,58 +33,122 @@ export const Ship = ({ ship }: { ship: ShipInterface }) => {
   const shipWidth = `${ship.width}mm`;
   const shipWidthPlus = `${ship.width + 2}mm`;
 
+  const shipLength = `${ship.length}mm`;
+
+  var displayFactionSize = "";
+  if (ship.hasOwnProperty("displayFactionSize")) {
+    //   displayFactionSize = `style="font-size:${ship.displayFactionSize}mm"`;
+    displayFactionSize = `font-size:${ship.displayFactionSize}mm;`;
+  }
+  var displayIconSize = "";
+  if (ship.hasOwnProperty("displayIconSize")) {
+    displayIconSize = ` style="font-size:${ship.displayIconSize}mm"`;
+  }
+
+  var factionColor = "";
+  if (useFactionColor) {
+    factionColor = `color:${faction.color}`;
+  }
+
+  var display = `<div class="icon shipFaction" style="${displayFactionSize} ${factionColor}">${faction.icon}</div>
+                <div class="fancy">${ship.name}</div>
+                <div class="ship"${displayIconSize}>${ship.icon}</div>`;
+
+  var displayTopFactionSize = "";
+  if (ship.hasOwnProperty("displayTopFactionSize")) {
+    displayTopFactionSize = `font-size:${ship.displayTopFactionSize}mm;`;
+  }
+
+  var displayTopIconSize = "";
+  if (ship.hasOwnProperty("displayTopIconSize")) {
+    displayTopIconSize = ` style="font-size:${ship.displayTopIconSize}mm"`;
+  }
+
+  var displayTop = "";
+  if (!ship.hasOwnProperty("displayTop") || ship.displayTop) {
+    displayTop = `<div class="icon shipFaction" style="${displayTopFactionSize} ${factionColor}">${faction.icon}</div>
+              <div class="fancy">${ship.name}</div>
+              <div class="ship"${displayTopIconSize}>${ship.icon}</div>`;
+  }
+
+  var displaySideFactionSize = "";
+  if (ship.hasOwnProperty("displaySideFactionSize")) {
+    displaySideFactionSize = `font-size:${ship.displaySideFactionSize}mm;`;
+  }
+
+  var displaySideIconSize = "";
+  if (ship.hasOwnProperty("displaySideIconSize")) {
+    displaySideIconSize = ` style="font-size:${ship.displaySideIconSize}mm"`;
+  }
+
+  var displaySide = "";
+  if (!ship.hasOwnProperty("displaySide") || ship.displaySide) {
+    displaySide = `<div class="icon shipFaction" style="${displaySideFactionSize} ${factionColor}">${faction.icon}</div>
+              <div class="fancy" style="width=${ship.height}mm;">${ship.name}</div>
+              <div class="ship"${displaySideIconSize}>${ship.icon}</div>`;
+  }
+
+  let sideLeftOffset = 0;
+  if (ship.sideLeftOffset || ship.displaySide) {
+    sideLeftOffset = ship.sideLeftOffset;
+  }
+  let sideRightOffset = sideLeftOffset;
+  if (ship.sideRightOffset || ship.displaySide) {
+    sideRightOffset = ship.sideRightOffset;
+  }
+
   return <div id="ship-${id}" className="box">
-	{/* <span className="removeButton no-print" onclick="remove(${id})">Remove</span> */}
-	<table>
-    {/* <tr className="top-flap">
-        <td className="model-height" style={{ width: shipHeight, maxWidth: shipHeight }}></td>
-        <td className="model-width render-flap cut-top cut-left cut-right" style={{width:shipWidth, maxWidth:shipWidth}}></td>
-        <td colSpan={3}></td>
-    </tr>
-      <tr className="top model-height-height" style={{ height: shipHeightPlus, maxHeight: shipHeightPlus }}>
-        <td className="model-height flap-left cut-left cut-right cut-top" style="width:${ship.height}mm; border-top-left-radius:${ship.height}mm; max-width:${ship.height}mm;"></td>
-        <td className="model-width render cut-left cut-right" style={{width:shipWidth, maxWidth:shipWidth}}>
-            <div className="upsideDown content">
-            ${displayTop}
-            </div>
-        </td>
-        <td className="model-height flap-right cut-left cut-right cut-top" style="width:${ship.height}mm; border-top-right-radius:${ship.height}mm; max-width:${ship.height}mm;"></td>
-        <td colSpan={2}></td>
-    </tr>
-    <tr className="main model-length"  style="height:${ship.length}mm; max-height:${ship.length}mm;">
-        <td className="model-height render cut-left" style={{ width: shipHeight, maxWidth: shipHeight }}>
-            <div className="leftSide sideDisplay" style="left:${sideLeftOffset}mm;">
-            	${displaySide}
-            </div>
-        </td>
-        <td className="model-width render" style={{width:shipWidth, maxWidth:shipWidth}}>
-            ${display}
-        </td>
-        <td className="model-height render" style={{ width: shipHeight, maxWidth: shipHeight }}>
-            <div className="rightSide sideDisplay" style="left:${sideRightOffset}mm;">
-            ${displaySide}
-            </div>
-        </td>
-        <td className="model-width render cut-top" style={{width:shipWidth, maxWidth:shipWidth}}>
-            ${display}
-        </td>
-        <td className="side-flap render cut-top cut-bottom cut-right glue"></td>
-        <td></td>
-    </tr>
-    <tr className="model-height-height" style="height:${ship.height + 2}mm;">
-        <td className="model-height render cut-left cut-bottom cut-right glue" style="width:${ship.height}mm; max-width:${ship.height}mm;">
-        </td>
-        <td className="model-width render cut-left cut-bottom cut-right" style="width:${ship.width}mm; max-width:${ship.width}mm;">
-        <div className="content">
-        ${displayTop}
-        </div>
-        </td>
-        <td className="model-height render cut-left cut-bottom cut-right glue" style="width:${ship.height}mm; max-width:${ship.height}mm;">
-        </td>
-        <td className="model-width render cut-left cut-bottom cut-right glue" style="width:${ship.width}mm; max-width:${ship.width}mm;">
-        </td>
-        <td></td>
-    </tr> */}
-</table>
-</div>
+    {/* <span className="removeButton no-print" onclick="remove(${id})">Remove</span> */}
+    <table>
+      <tr className="top-flap">
+          <td className="model-height" style={{ width: shipHeight, maxWidth: shipHeight }}></td>
+          <td className="model-width render-flap cut-top cut-left cut-right" style={{width:shipWidth, maxWidth:shipWidth}}></td>
+          <td colSpan={3}></td>
+      </tr>
+        <tr className="top model-height-height" style={{ height: shipHeightPlus, maxHeight: shipHeightPlus }}>
+          <td className="model-height flap-left cut-left cut-right cut-top" style={{width:shipHeight, borderTopLeftRadius:shipHeight, maxWidth:shipHeight}}></td>
+          <td className="model-width render cut-left cut-right" style={{width:shipWidth, maxWidth:shipWidth}}>
+              <div className="upsideDown content">
+              ${displayTop}
+              </div>
+          </td>
+          <td className="model-height flap-right cut-left cut-right cut-top" style={{width:shipHeight, borderTopRightRadius:shipHeight, maxWidth:shipHeight}}></td>
+          <td colSpan={2}></td>
+      </tr>
+      <tr className="main model-length"  style={{height:shipLength, maxHeight:shipLength}}>
+          <td className="model-height render cut-left" style={{ width: shipHeight, maxWidth: shipHeight }}>
+              <div className="leftSide sideDisplay" style={{left:`${sideLeftOffset}mm`}}>
+                ${displaySide}
+              </div>
+          </td>
+          <td className="model-width render" style={{width:shipWidth, maxWidth:shipWidth}}>
+              ${display}
+          </td>
+          <td className="model-height render" style={{ width: shipHeight, maxWidth: shipHeight }}>
+              <div className="rightSide sideDisplay" style={{left:`${sideRightOffset}mm`}}>
+              ${displaySide}
+              </div>
+          </td>
+          <td className="model-width render cut-top" style={{width:shipWidth, maxWidth:shipWidth}}>
+              ${display}
+          </td>
+          <td className="side-flap render cut-top cut-bottom cut-right glue"></td>
+          <td></td>
+      </tr>
+      <tr className="model-height-height" style={{height:shipHeightPlus}}>
+          <td className="model-height render cut-left cut-bottom cut-right glue" style={{ width: shipHeight, maxWidth: shipHeight }}>
+          </td>
+          <td className="model-width render cut-left cut-bottom cut-right" style={{width:shipWidth, maxWidth:shipWidth}}>
+          <div className="content">
+          ${displayTop}
+          </div>
+          </td>
+          <td className="model-height render cut-left cut-bottom cut-right glue" style={{width:shipHeight, maxWidth:shipHeight}}>
+          </td>
+          <td className="model-width render cut-left cut-bottom cut-right glue" style={{width:shipWidth, maxWidth:shipWidth}}>
+          </td>
+          <td></td>
+      </tr>
+    </table>
+  </div>
 }
--- src/components/ships.tsx
+++ src/components/ships.tsx
@@ -1,49 +1,33 @@
 import React, { useState } from "react";
 import { factions, ships } from './data';
-import { Ship, ShipInterface } from "./ship";
+import { FactionInterface, Ship, ShipInterface } from "./ship";
 
 export const Ships = () => {
 
-  const defaultFaction = factions[0].name;
-  const defaultShip = ships.find(ship => ship.factions.includes(defaultFaction))?.name;
+  const defaultFaction = factions[0];
+  const defaultShip = ships.find(ship => ship.factions.includes(defaultFaction.name));
 
-  const [selectedFaction, setSelectedFaction] = useState(defaultFaction);
-  const [selectedShip, setSelectedShip] = useState(defaultShip);
+  const [selectedFaction, setSelectedFaction] = useState<FactionInterface>(defaultFaction);
+  const [selectedShip, setSelectedShip] = useState<ShipInterface>(defaultShip);
   const [addedShips, setAddedShips] = useState<ShipInterface[]>([]);
 
-  const selectFaction = (event: any) => {
-    setSelectedFaction(event.target.value);
-  }
-
-  const selectShip = (event: any) => {
-    setSelectedShip(event.target.value);
-  }
-
-  const addShip = () => {
-    const shipToAdd = ships.find(ship => ship.name === selectedShip);
-    if (shipToAdd) {
-      // @ts-ignore
-      setAddedShips([...addedShips, shipToAdd]);
-    }
-  }
-
-  const removeShip = (index: number) => {
-    const updatedShips = addedShips.splice(index, 1);
-    setAddedShips(updatedShips);
-  }
+  const selectFaction = (event: any) => setSelectedFaction(factions.find(faction => faction.name === event.target.value));
+  const selectShip = (event: any) => setSelectedShip(event.target.value);
+  const addShip = () => setAddedShips([...addedShips, ships.find(ship => ship.name === selectedShip) as ShipInterface]);
+  const removeShip = (index: number) => setAddedShips([...addedShips.slice(0, index), ...addedShips.slice(index + 1)]);
 
   return <div>
     <div className="no-print">
       <input id="useFactionColor" type="checkbox" />
       <label htmlFor="useFactionColor">Use faction colors</label>
-      <select onChange={selectFaction} defaultValue={defaultFaction}>
+      <select onChange={selectFaction} defaultValue={defaultFaction.name}>
         {factions.map(faction =>
           <option value={faction.name}>{faction.name}</option>
         )}
       </select>
-      <select onChange={selectShip} defaultValue={defaultShip}>
+      <select onChange={selectShip} defaultValue={defaultShip.name}>
         {ships
-          .filter(ship => ship.factions.includes(selectedFaction))
+          .filter(ship => ship.factions.includes(selectedFaction.name))
           .map(ship => <option value={ship.name}>{ship.fullName}</option>)
         }
       </select>
@@ -51,7 +35,7 @@ export const Ships = () => {
       {addedShips.map((ship, index) =>
         <div>
           <span className="removeButton no-print" onClick={() => removeShip(index)}>Remove {ship.fullName}</span>
-          <Ship ship={ship} />
+          <Ship ship={ship} faction={selectedFaction} useFactionColor={true} />
         </div>
       )}
     </div>
