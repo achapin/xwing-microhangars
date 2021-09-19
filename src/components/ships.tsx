7514b3654e232ccc5ab55c9b3c94c4cf3e82d755
Phillip Manwaring <phillip.manwaring@gmail.com>
2021-09-19 00:25:27 -0400

    update libs

--- src/components/ships.tsx
+++ src/components/ships.tsx
@@ -1,41 +1,51 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { factions, ships } from './data';
 import { Ship } from "./ship";
 
+interface AddedShip {
+  ship;
+  faction;
+}
+
+interface ShipsForm {
+  faction: string;
+  ship: string;
+}
+
 export const Ships = () => {
 
-  const defaultFaction = factions[0];
-  const defaultShip = ships.find(ship => ship.factions.includes(defaultFaction.name));
+  const { handleSubmit, register } = useForm<ShipsForm>();
 
-  const [selectedFaction, setSelectedFaction] = useState(defaultFaction);
-  const [selectedShip, setSelectedShip] = useState(defaultShip);
-  const [addedShips, setAddedShips] = useState([]);
+  const [addedShips, setAddedShips] = useState<AddedShip[]>([]);
 
-  const selectFaction = (event: any) => setSelectedFaction(factions.find(faction => faction.name === event.target.value));
-  const selectShip = (event: any) => setSelectedShip(event.target.value);
-  const addShip = () => setAddedShips([...addedShips, ships.find(ship => ship.name === selectedShip.name)]);
+  const addShip = ({ ship, faction}: ShipsForm) => setAddedShips([...addedShips, { ship, faction }]);
   const removeShip = (index: number) => setAddedShips([...addedShips.slice(0, index), ...addedShips.slice(index + 1)]);
 
   return <div>
     <div className="no-print">
       <input id="useFactionColor" type="checkbox" />
       <label htmlFor="useFactionColor">Use faction colors</label>
-      <select onChange={selectFaction} defaultValue={defaultFaction.name}>
-        {factions.map(faction =>
-          <option value={faction.name}>{faction.name}</option>
-        )}
-      </select>
-      <select onChange={selectShip} defaultValue={defaultShip.name}>
-        {ships
-          .filter(ship => ship.factions.includes(selectedFaction.name))
-          .map(ship => <option value={ship.name}>{ship.fullName}</option>)
-        }
-      </select>
-      <button onClick={addShip}>Add ship</button>
-      {addedShips.map((ship, index) =>
+
+      <form onSubmit={handleSubmit(addShip)}>
+        <select {...register("faction")}>
+          {factions.map(faction =>
+            <option value={faction.name}>{faction.name}</option>
+          )}
+        </select>
+        <select {...register("ship")}>
+          {ships
+            // .filter(ship => ship.factions.includes(selectedFaction.name))
+            .map(ship => <option value={ship.name}>{ship.fullName}</option>)
+          }
+        </select>
+        <button type="submit">Add ship</button>
+      </form>
+      
+      {addedShips.map(({ ship, faction }, index) =>
         <div>
           <span className="removeButton no-print" onClick={() => removeShip(index)}>Remove {ship.fullName}</span>
-          <Ship ship={ship} faction={selectedFaction} useFactionColor={true} />
+          <Ship ship={ship} faction={faction} useFactionColor={true} />
         </div>
       )}
     </div>
